a0230c647edb8678f2f393bc538cb6405458af35
Hugo Vila <git@hugovila.nom.es>
2014-03-27 23:25:13 +0000

    Added /bonjour/loquesea

--- site.rb
+++ site.rb
@@ -11,10 +11,26 @@ get '/' do
   erb:index
 end
 
-get '/read_more' do
-  erb:read_more
-end
+# get '/read_more' do
+#  erb:read_more
+# end
 
 get '/italian' do
   "Ciao!"
+end
+
+get '/spanish' do
+  "Adios!"
+end
+
+get '/bonjour/:nom' do |n|
+  "Bonjour #{n} !"
+end
+
+get '/read_more' do
+  erb:read_more, :layout => :read
+end
+
+error 403 do
+  "Acceso prohibido"
 end
